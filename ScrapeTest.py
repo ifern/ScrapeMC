3b2cc53e38186967efece1f8aead15f87ea5f9db
Inigo Fernando <inigogct@gmail.com>
2015-06-30 12:06:52 +0530

    ScrapemyIRIS
    Scrape research call from myiris.com

---
+++ ScrapeTest.py
@@ -0,0 +1,38 @@
+import urllib
+import csv
+import requests
+import io
+import re
+import sys
+
+wordsfile = csv.DictReader(open("words.csv"))
+
+
+#Search for every word in the words list in every URL in the links list. Print both word and the URL if found
+
+for word in wordsfile:
+	curr_word = word['search_word']
+
+	linksfile = csv.DictReader(open("links.csv"))
+
+	for link in  linksfile:
+		curr_link = link['url']
+		site = urllib.urlopen(curr_link).read()
+
+        	if curr_word in site:
+			text_file = open("Output.txt", "w")
+			text_file.write(site)
+			text_file.close()
+	f = open("Output.txt",'r')
+	
+	for line in f:
+	#flag=1
+	#while(flag):
+		#line = f.read(1)
+		if re.search(curr_word,line):
+			print(line)
+			break
+
+			#print (type(site))
+			#print (site)
+            		#print(curr_word, " Present in", curr_link)
